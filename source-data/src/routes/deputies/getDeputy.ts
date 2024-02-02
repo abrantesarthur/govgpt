01ebbf09b7186ebab0f9db24a5015a52a669c60a
Arthur Abrantes <abrantesarthur1997@gmail.com>
2024-02-01 22:58:56 -0800

    add comments to getDeputy

--- source-data/src/routes/deputies/getDeputy.ts
+++ source-data/src/routes/deputies/getDeputy.ts
@@ -1,6 +1,12 @@
 import axios from "axios"
 import { GET_ALL_DEPUTIES_URL } from "./constants";
 
+/**
+ * get a Deputy
+ * 
+ * @param param: the parameters to filter the deputy
+ * @returns a deputy's information
+ */
 export const getDeputy = async ({
     id,
 }: {
