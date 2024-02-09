f01d11b5d6ac4bdd088c8df04d77b425b1929557
Arthur Abrantes <abrantesarthur1997@gmail.com>
2024-02-08 20:58:10 -0800

    create the assistant package

--- source-data/src/main.ts
+++ source-data/src/main.ts
@@ -1,6 +1,6 @@
 import { buildDeputyExpensesFile } from "./file-builders";
 
-
+// TODO: use some key management system to store the OpenAI api key
 const main = async () => {
     await buildDeputyExpensesFile();
 }
